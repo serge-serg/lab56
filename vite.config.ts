7e10a196bab2e70f79e472738b8a0b8d2e99b20b
serge-serg <srgg67@gmail.com>
2025-11-11 21:57:47 +0200

    Tuning config

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,40 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-    base: "/frontend",
-    server: {
-        host: true,
-        port: 3000,
-        proxy: {
-            "/api": {
-                target: "http://localhost:8000"
-            },
-            "/images": {
-                target: "http://localhost:9000"
-            }
-        },
+export default defineConfig(({ mode }) => ({
+  base: "/frontend",
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+      },
+      "/images": {
+        target: "http://localhost:9000",
+      },
     },
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Lab5 App',
-                short_name: 'Lab5',
-                start_url: '/lab56/',
-                display: 'standalone',
-                background_color: '#ffffff',
-                theme_color: '#0d6efd',
-                icons: [
-                    { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
-                    { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' }
-                ]
-            }
-        })
-    ]
-})
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Lab5 App",
+        short_name: "Lab5",
+        start_url: mode === 'development' ? "/" : "/lab56/",
+        scope: mode === 'development' ? "/" : "/lab56/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0d6efd",
+        icons: [
+          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
+}));
