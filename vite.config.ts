b0f60b87dcfb72f4a06c80c6ccaa33d22fd0e258
serge-serg <srgg67@gmail.com>
2025-11-11 19:45:29 +0200

    Adding contents for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     base: "/frontend",
@@ -18,6 +19,21 @@ export default defineConfig({
     },
     plugins: [
         react(),
-        tsconfigPaths()
+        tsconfigPaths(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Lab5 App',
+                short_name: 'Lab5',
+                start_url: '/lab56/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#0d6efd',
+                icons: [
+                    { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
+                    { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' }
+                ]
+            }
+        })
     ]
 })
